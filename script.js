cb00fa079bcf1f1ce5cb60d72a01e003f00391e2
GomesGuilhermePT07 <gomesguilherme.pt@gmail.com>
2025-03-08 01:13:25 +0000

    js done

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 const localStorageName = 'to-do-list-gg'
 
+function validateIfExistsNewTask(){
+    let values = JSON.parse(localStorage.getItem(localStorageName) || "[]")
+    let inputValue = document.getElementById('input-new-task').value
+    let exists = values.find(x => x.name == inputValue)
+    return !exists ? false : true
+}
+
 function newTask(){
     let input = document.getElementById('input-new-task')
-    
+    input.style.border = ""
+
     // Validation
     if(!input.value){
+        input.style.border = "3px solid red"
         alert('Please enter a task')
     } 
-    // else if() {
-
-    // }
+    else if(validateIfExistsNewTask()) {
+        alert('Task already created!')
+    }
     else {
         // Increment to localStorage
         let values = JSON.parse(localStorage.getItem(localStorageName) || "[]")
@@ -19,6 +28,7 @@ function newTask(){
         localStorage.setItem(localStorageName,JSON.stringify(values))
         showValues()
     }
+    input.value = ''
 }
 
 function showValues(){
@@ -27,13 +37,17 @@ function showValues(){
     list.innerHTML = ''
 
     for(let i=0; i<values.length; i++){
-        list.innerHTML += `<li>${values[i]['name']}<button id="btn-ok" onclick="removeItem("${values[i]['name']}")">ok</button></li>`
+        list.innerHTML += `<li>${values[i]['name']}<button id="btn-ok" onclick='removeItem("${values[i]['name']}")'>
+                           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+                           <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
+                           </svg>
+                           </button></li>`
     }
 }
 
 function removeItem(data){
     let values = JSON.parse(localStorage.getItem(localStorageName) || "[]")  
-    let index = values.findIndex(x => x.name === data)
+    let index = values.findIndex(x => x.name == data)
     values.splice(index,1)
     localStorage.setItem(localStorageName,JSON.stringify(values))
     showValues()
