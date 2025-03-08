e7fdda4a68424c0f04badb4bbbbfbde8de4b7b41
GomesGuilhermePT07 <gomesguilherme.pt@gmail.com>
2025-03-08 00:32:57 +0000

    add

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const localStorageName = 'to-do-list-gg'
+
 function newTask(){
-    alert('teste');
+    let input = document.getElementById('input-new-task');
+    
+    // Validation
+    if(!input.value){
+        alert('Please enter a task');
+    } 
+    // else if() {
+
+    // }
+    else {
+        // Increment to localStorage
+        let values = JSON.parse(localStorage.getItem('localStorageName') || "[]");
+        values.push({
+            name: input.value
+        });
+        localStorage.setItem(localStorageName,values);
+    }
 }
