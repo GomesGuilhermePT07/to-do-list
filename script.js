44f1ac8e3b30f4b99d1c91a0c61f650d1afd2fb5
GomesGuilhermePT07 <gomesguilherme.pt@gmail.com>
2025-03-08 00:52:51 +0000

    add

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
 const localStorageName = 'to-do-list-gg'
 
 function newTask(){
-    let input = document.getElementById('input-new-task');
+    let input = document.getElementById('input-new-task')
     
     // Validation
     if(!input.value){
-        alert('Please enter a task');
+        alert('Please enter a task')
     } 
     // else if() {
 
     // }
     else {
         // Increment to localStorage
-        let values = JSON.parse(localStorage.getItem('localStorageName') || "[]");
+        let values = JSON.parse(localStorage.getItem(localStorageName) || "[]")
         values.push({
             name: input.value
-        });
-        localStorage.setItem(localStorageName,JSON.stringify(values));
+        })
+        localStorage.setItem(localStorageName,JSON.stringify(values))
+        showValues()
     }
 }
 
 function showValues(){
     let values = JSON.parse(localStorage.getItem(localStorageName) || "[]");
-    let list = document.getElementById('to-do-list');
-    list.innerHTML = '';
+    let list = document.getElementById('to-do-list')
+    list.innerHTML = ''
 
     for(let i=0; i<values.length; i++){
-        list.innerHTML= `<li>${values[i]['name']}</li>`
+        list.innerHTML += `<li>${values[i]['name']}<button id="btn-ok" onclick="removeItem("${values[i]['name']}")">ok</button></li>`
     }
 }
+
+function removeItem(data){
+    let values = JSON.parse(localStorage.getItem(localStorageName) || "[]")  
+    let index = values.findIndex(x => x.name == data)
+}
+
+showValues()
